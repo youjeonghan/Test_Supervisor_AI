e07756b9d787fd91f6158739f805e3b5a5108424
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-12-19 22:58:24 +0900

    수정

--- Detection_Cheating/app.py
+++ Detection_Cheating/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from api_v1 import api
+import config
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 app=Flask(__name__)
 app.config.from_object(config)
+
+db.init_app(app)
+migrate.init_app(app, db)
 
 app.register_blueprint(api, url_prefix="/api")
 
--- Detection_Cheating/config.py
+++ Detection_Cheating/config.py
@@ -1,2 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'students.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 ADMIN_ID = "Manager"
 ADMIN_PW = "1234"
